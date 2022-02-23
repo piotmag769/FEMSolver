53248dead6251e33645eb977a11c345bd7166b43
Piotr Magiera <piotrmagiera150@gmail.com>
2022-02-23 02:18:41 +0100

    adding mathematical paper + small changes

--- solver.py
+++ solver.py
@@ -3,7 +3,7 @@ from numpy.linalg import solve
 from scipy.integrate import quad
 from matplotlib.pyplot import plot, show
 
-# global variables (yea, i could pass them to functions, but w/e)
+# global variables
 # provide n >= 2 (n = 1 doesn't make sense)
 n = int(input("provide n value\n"))
 h = 3 / n
@@ -37,9 +37,12 @@ B_matrix = [[0 for _ in range(n - 1)] for _ in range(n - 1)]
 B_func = lambda i, j: quad(lambda x: -1 / h ** 2, x_i(i - 1), x_i(i + 1))[0] if i == j else \
     quad(lambda x: 1 / h ** 2, x_i(min(i, j)), x_i(min(i, j) + 1))[0]
 
+a = B_func(1, 1)
+b = B_func(1, 2)
+
 for i in range(n - 1):
     for j in range(max(0, i - 1), min(i + 2, n - 1)):
-        B_matrix[i][j] = B_func(i, j)
+        B_matrix[i][j] = a if i == j else b
 
 
 L_matrix = [0 for _ in range(n - 1)]
